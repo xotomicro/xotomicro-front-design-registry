7892ad5877b4d7ae3eb083438aded4bd78fbd9e9
xotosphere <xotosphere@gmail.com>
2022-05-28 16:31:40 +0200

    blast off 🚀

---
+++ config/webpack.dev.js
@@ -0,0 +1,27 @@
+const {merge} = require("webpack-merge")
+const path = require("path")
+const commonConfig = require("./webpack.common")
+
+// eslint-disable-next-line no-undef
+__webpack_base_uri__ = "http://localhost:8080"
+
+const devConfig = {
+    mode: "development",
+
+    entry: "./src/start.ts",
+    output: {
+        path: path.resolve(process.cwd(), "dist"),
+        filename: "index.js",
+    },
+
+    devServer: {
+        port: process.env.SERVICE_PORT,
+        historyApiFallback: {
+            index: "index.html",
+        },
+    },
+
+    plugins: [],
+}
+
+module.exports = merge(commonConfig, devConfig)
