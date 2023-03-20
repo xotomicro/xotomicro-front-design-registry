dd9321adbb7567b6d8084a913ebf605ebbc6c104
xotosphere <xotosphere@gmail.com>
2023-03-20 21:05:49 +0100

    blast off 🚀

---
+++ config/webpack.common.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin")
+const {getAliasConfig} = require("@xotomicro/xotomicro-front-utility-registry/src/config/webpack.shared")
+
+module.exports = {
+    resolve: {
+        alias: getAliasConfig(),
+        extensions: [".js", ".tsx", ".jsx", ".ts"],
+        plugins: [new TsconfigPathsPlugin()],
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader",
+            },
+            {
+                test: /\.tsx?$/,
+                loader: "babel-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+        ],
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+        }),
+    ],
+}
